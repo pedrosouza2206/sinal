ac10e09e6b71d9407e6d4d4257dba7e05ea5af32
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 20:19:55 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿double numero;
+
+Console.Write("Digite um número: ");
+numero = Convert.ToDouble(Console.ReadLine());
+ 
+if (numero < 0)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Você digitou um número negativo.");
+    Console.ResetColor();
+}
+else if (numero > 0)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Você digitou um número positivo.");
+    Console.ResetColor();
+}
+
+else
+{
+     Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("Você digitou zero");
+    Console.ResetColor();
+}
